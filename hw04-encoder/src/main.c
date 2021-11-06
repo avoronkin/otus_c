c26401110cfa4bb8ac19df1c9731ba80d9caa0a4
alex <admin@elasticweb.ru>
2021-11-06 19:49:38 +0300

    hw04-encoder

--- hw04-encoder/src/main.c
+++ hw04-encoder/src/main.c
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
     Config config = create_config(args);
 
     int ch;
-    int wch;
+    int return_code;
     while ((ch = fgetc(config.input)) != EOF)
     {
 
         if (ch <= 127)
         {
-            wch = fputc(ch, config.output);
-            assert(wch != EOF && "Ошибка записи в выходной файл");
+            return_code = fputc(ch, config.output);
+            assert(return_code != EOF && "Ошибка записи в выходной файл");
         }
         else if (ch >= 128 && ch <= 255)
         {
@@ -23,17 +23,20 @@ int main(int argc, char *argv[])
 
             // Кодируем utf код (0x0420 1000010000) в utf8 (0xd0a0 1101000010100000)
             // Убираем младшие 6 бит, накладываем маску (0xc0 0b11000000)
-            wch = fputc((utf_code >> 6) | 0xC0, config.output);
-            assert(wch != EOF && "Ошибка записи в выходной файл");
+            return_code = fputc((utf_code >> 6) | 0xC0, config.output);
+            assert(return_code != EOF && "Ошибка записи в выходной файл");
 
             // Берём младшие 6 бит (0x3F 0b00111111), накладываем маску (0x80 0b10000000()
-            wch = fputc((utf_code & 0x3F) | 0x80, config.output);
-            assert(wch != EOF && "Ошибка записи в выходной файл");
+            return_code = fputc((utf_code & 0x3F) | 0x80, config.output);
+            assert(return_code != EOF && "Ошибка записи в выходной файл");
         }
     }
 
-    assert(fclose(config.input) == 0 && "Ошибка закрытия входного файла");
-    assert(fclose(config.output) == 0 && "Ошибка закрытия выходного файла");
+    return_code = fclose(config.input);
+    assert(return_code == 0 && "Ошибка закрытия входного файла");
+
+    return_code = fclose(config.output);
+    assert(return_code == 0 && "Ошибка закрытия выходного файла");
 
     exit(EXIT_SUCCESS);
 }
